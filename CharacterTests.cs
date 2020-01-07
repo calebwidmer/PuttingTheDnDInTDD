ae03235804678ca1dddab91c7d9ccaa310aa5ee0
CalebWidmer <widmersoftworks@gmail.com>
2020-01-07 11:41:42 -0500

    ttdTest

--- CharacterTests.cs
+++ CharacterTests.cs
@@ -49,24 +49,66 @@ namespace TestPuttingTheDnDInTDD
         {
             Character character = new Character();
 
-            Assert.True(character.Roll()>0);
+            Assert.True(character.Roll(1)>0);
          
 
 
         }
         [Fact]
-        public void CharacterCanHit()
+        public void CharacterCanBeat()
         {
             Character protagonist = new Character();
             Character antagonist = new Character();
-            antagonist.ArmorClass = 14;
+            //antagonist.ArmorClass = 14;
+            Assert.True(protagonist.CanHit(antagonist, antagonist.ArmorClass+1));
+
+        }
+
+        [Fact]
+        public void CharacterCanMeet()
+        {
+            Character protagonist = new Character();
+            Character antagonist = new Character();
+
+            Assert.True(protagonist.CanHit(antagonist, antagonist.ArmorClass));
+
+        }
+
+        [Fact]
+        public void CharacterCanMiss()
+        {
+            Character protagonist = new Character();
+            Character antagonist = new Character();
+
+            Assert.False(protagonist.CanHit(antagonist, antagonist.ArmorClass - 1));
+
+        }
 
 
-            
-            Assert.True(protagonist.CanHit(antagonist));
+        //If a roll is a natural 20 then a critical hit is dealt and the damage is doubled
+        [Fact]
+        public void CharacterCanDamage()
+        {
+            Character protagonist = new Character();
+            Character antagonist = new Character();
+            int initialHitPoints = antagonist.HitPoints;
 
+            protagonist.Wound(antagonist, 1);
 
+            Assert.True(initialHitPoints-1 == antagonist.HitPoints );
 
+        }
+
+        [Fact]
+        public void CriticalHitDoublesDamage()
+        {
+            Character protagonist = new Character();
+            Character antagonist = new Character();
+            int initialHitPoints = antagonist.HitPoints;
+
+            protagonist.PlayerTurn(antagonist, 20);
+
+            Assert.True(initialHitPoints - 2 == antagonist.HitPoints);
         }
     }
 }
